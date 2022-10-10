9dd23dc222582e6c04195bdbf3682f5874084a84
wathenjiang <wathenjiang@tencent.com>
2022-10-10 11:20:56 +0800

    add comments

--- sonyflake.go
+++ sonyflake.go
@@ -16,8 +16,11 @@ import (
 
 // These constants are the bit lengths of Sonyflake ID parts.
 const (
-	BitLenTime      = 39                               // bit length of time
-	BitLenSequence  = 8                                // bit length of sequence number
+	// BitLenTime 39 位的时间戳，单位为 10ms
+	BitLenTime = 39 // bit length of time
+	// BitLenSequence 8 位，每 10 ms 内最多有 256 个记录
+	BitLenSequence = 8 // bit length of sequence number
+	// BitLenMachineID 机器 ID，这里也就是 16 位，共可以支持 65536 台机器
 	BitLenMachineID = 63 - BitLenTime - BitLenSequence // bit length of machine id
 )
 
@@ -36,18 +39,21 @@ const (
 // If CheckMachineID returns false, Sonyflake is not created.
 // If CheckMachineID is nil, no validation is done.
 type Settings struct {
-	StartTime      time.Time
-	MachineID      func() (uint16, error)
+	// StartTime 开始时间
+	StartTime time.Time
+	// MachineID 机器 ID 获取方法，如果为 nil，那么默认取 private IP 后 16 位
+	MachineID func() (uint16, error)
+	// CheckMachineID 检查 ID 的合法性
 	CheckMachineID func(uint16) bool
 }
 
 // Sonyflake is a distributed unique ID generator.
 type Sonyflake struct {
 	mutex       *sync.Mutex
-	startTime   int64
-	elapsedTime int64
-	sequence    uint16
-	machineID   uint16
+	startTime   int64  // 开始时间
+	elapsedTime int64  // 已经过去的时间，用于检查是否有时间回拨现象
+	sequence    uint16 // 某一个时刻，可以总共生产 256 个序号（2^8），但是使用 uint16 位保存，是因为可能存在时间回拨现象
+	machineID   uint16 // 机器号
 }
 
 // NewSonyflake returns a new Sonyflake configured with the given Settings.
@@ -86,19 +92,25 @@ func NewSonyflake(st Settings) *Sonyflake {
 // After the Sonyflake time overflows, NextID returns an error.
 func (sf *Sonyflake) NextID() (uint64, error) {
 	const maskSequence = uint16(1<<BitLenSequence - 1)
-
+	// 上全局锁
 	sf.mutex.Lock()
 	defer sf.mutex.Unlock()
-
+	// 获取当前时间到 startTime 的时间序列
 	current := currentElapsedTime(sf.startTime)
 	if sf.elapsedTime < current {
+		// sf.elapsedTime < current 没有出现时间回拨现象，处于新的时间轮次，需要将 sequence 重新置为 0
 		sf.elapsedTime = current
 		sf.sequence = 0
 	} else { // sf.elapsedTime >= current
 		sf.sequence = (sf.sequence + 1) & maskSequence
+		// sf.sequence == 0 有两种情况
+		// 情况1：sf.elapsedTime == current && sf.sequence == 0，说明此 sf.elapsedTime 时刻的 256 个序号被消耗完毕了，因此等待一个时间单位（sf.elapsedTime++）
+		// 情况2：sf.elapsedTime > current && sf.sequence == 0，说明出现了时间回拨现象，因此等待，直到机器时间等于 sf.elapsedTime + 1
 		if sf.sequence == 0 {
-			sf.elapsedTime++
+			sf.elapsedTime++ // 两种情况都会进行 sf.elapsedTime++，这是最关键的
 			overtime := sf.elapsedTime - current
+			// 为了避免时间回拨，sleep 这些时间
+			// NOTE：此时锁没有释放，当前协程 sleep 期间，其余调用 Sonyflake.NextID 的方法也会阻塞于锁，相当于继续等待
 			time.Sleep(sleepTime((overtime)))
 		}
 	}
@@ -150,7 +162,6 @@ func privateIPv4() (net.IP, error) {
 	}
 	return nil, errors.New("no private ip address")
 }
-
 func isPrivateIPv4(ip net.IP) bool {
 	return ip != nil &&
 		(ip[0] == 10 || ip[0] == 172 && (ip[1] >= 16 && ip[1] < 32) || ip[0] == 192 && ip[1] == 168)
@@ -166,21 +177,25 @@ func lower16BitPrivateIP() (uint16, error) {
 }
 
 // ElapsedTime returns the elapsed time when the given Sonyflake ID was generated.
+// 得到此 ID 距 startTime 已经有多少时间了
 func ElapsedTime(id uint64) time.Duration {
 	return time.Duration(elapsedTime(id) * sonyflakeTimeUnit)
 }
 
+// 将 ID 向右移动 8 + 16 位，获取得到 ID 的前 40 位（首位一直为 0），因此相当于获取 39 位的时间戳数值
 func elapsedTime(id uint64) uint64 {
 	return id >> (BitLenSequence + BitLenMachineID)
 }
 
 // SequenceNumber returns the sequence number of a Sonyflake ID.
+// 返回序列 ID
 func SequenceNumber(id uint64) uint64 {
 	const maskSequence = uint64((1<<BitLenSequence - 1) << BitLenMachineID)
 	return id & maskSequence >> BitLenMachineID
 }
 
 // MachineID returns the machine ID of a Sonyflake ID.
+// 返回机器 ID
 func MachineID(id uint64) uint64 {
 	const maskMachineID = uint64(1<<BitLenMachineID - 1)
 	return id & maskMachineID
@@ -193,10 +208,10 @@ func Decompose(id uint64) map[string]uint64 {
 	sequence := SequenceNumber(id)
 	machineID := MachineID(id)
 	return map[string]uint64{
-		"id":         id,
-		"msb":        msb,
-		"time":       time,
-		"sequence":   sequence,
-		"machine-id": machineID,
+		"id":         id,        // 整个 ID
+		"msb":        msb,       // 首位
+		"time":       time,      // 时间戳
+		"sequence":   sequence,  // 序列号
+		"machine-id": machineID, // 机器 ID
 	}
 }
